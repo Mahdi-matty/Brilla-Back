a11a9902a17ac59db7e4c2a91a63c00097137c82
David Rodriguez <santiago1.dsrr@gmail.com>
2024-01-18 19:40:21 -0500

    Changed file names

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const Teacher = require('./Teacher');
 const Student = require('./Student');
 const Subject = require(`./Subject`);
-const Topic = require(`./Topic`);
+const Topic = require('./Topic');
 const Card = require('./Card')
 
 // students and subjects
