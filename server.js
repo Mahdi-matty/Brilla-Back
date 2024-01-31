868ae7aeeb6c10e89b6198b41ea0a7bab2bbab7d
David Rodriguez <santiago1.dsrr@gmail.com>
2024-01-30 21:18:27 -0500

    added transporter inside helpers folder

--- server.js
+++ server.js
@@ -9,33 +9,32 @@ const cors = require('cors');
 app.use(cors());
 
 app.use(bodyParser.json())
-app.post('/api/send-email', async (req, res) => {
-    const { email, authCode } = req.body;
-
-const transporter = nodemailer.createTransport({
-        // host: "smtp.forwardemail.net",
-        // port: 465,
-        service: 'aol',
-        auth: {
-          user: 'brillamentee@aol.com',
-          pass: process.env.mailPass,
-        },
-      });
-    try {
-        await transporter.sendMail({
-          from: 'brillamentee@aol.com',
-          to: email,
-          subject: 'Authentication Code',
-          text: `Your authentication code is: ${authCode}`,
-        });
+// app.post('/api/send-email', async (req, res) => {
+//     const { email, authCode } = req.body;
+//  const transporter = nodemailer.createTransport({
+//         // host: "smtp.forwardemail.net",
+//         // port: 465,
+//         service: 'aol',
+//         auth: {
+//           user: 'brillamentee@aol.com',
+//           pass: process.env.mailPass,
+//         },
+//       });
+//     try {
+//         await transporter.sendMail({
+//           from: 'brillamentee@aol.com',
+//           to: email,
+//           subject: 'Authentication Code',
+//           text: `Your authentication code is: ${authCode}`,
+//         });
     
-        console.log('Email sent with authentication code');
-        res.sendStatus(200);
-      } catch (error) {
-        console.error('Error sending email:', error);
-        res.status(500).json({ error: 'Error sending email. Please try again.' });
-      }
-    });
+//         console.log('Email sent with authentication code');
+//         res.sendStatus(200);
+//       } catch (error) {
+//         console.error('Error sending email:', error);
+//         res.status(500).json({ error: 'Error sending email. Please try again.' });
+//       }
+//     });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
