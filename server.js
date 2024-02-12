28c0ec1dd467943781f69db2869c3710f14cd6e7
David Rodriguez <santiago1.dsrr@gmail.com>
2024-02-12 15:26:33 -0500

    changed server file

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ const CLIENT_SECRET = process.env.CLIENT_SEC
 // });
 app.get('/getAccessToken', async function(req, res){
     console.log(req.query.code);
-    const params = '?client_id='+ CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '$code=' + req.query.code;
+    const params = '?client_id='+ CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&code=' + req.query.code;
     await fetch('https://github.com/login/oauth/access_token'+ params, {
             method: 'POST',
             headers: {
